fc64fe6b5e70123b0a9fc327550151f082a62a40
muxspace <rowe@nurometic.com>
2011-03-08 15:52:22 -0500

    Added duplicate record declaration check

--- include/sparse_matrix.hrl
+++ include/sparse_matrix.hrl
@@ -1,8 +1,10 @@
+-ifndef(SPARSE_MATRIX_HRL).
+-define(SPARSE_MATRIX_HRL,1).
 %% dims - The dimensions of the matrix
 %% default - The value to use if a point does not exist
 %% strict - Whether to strictly adhere to dimensions or allow any coordinates
 %%   to be accessed
 %% values - Values are in a modified triplet form: {{R,C}, V}
 -record(sparse_matrix, {dims, default=0, strict=false, values=[]}).
-
+-endif.
 
