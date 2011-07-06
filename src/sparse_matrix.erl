0363cf80e0e0e3cc8abbcdde0891abaf9c9690bd
Yuri Cho <yuri@fozzie.(none)>
2011-07-06 14:35:39 -0400

    refactored with brian's feedback

--- src/sparse_matrix.erl
+++ src/sparse_matrix.erl
@@ -1,6 +1,6 @@
 -module(sparse_matrix).
 -export([new/0, new/1, new/2,
-  from_triplet/1, from_triplet/2, from_triplet/3,
+  from_triplet/1, from_triplet/2,
   from_list/1, from_list/2, from_list/3,
   get/2, put/2, delete/2, coordinates/1, dimensions/1,
   add/2, mult/2 ]).
@@ -9,46 +9,70 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CONSTRUCTORS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 new() -> #sparse_matrix{}.
 new(Default) -> #sparse_matrix{default=Default}.
-new(Default,Sym) -> #sparse_matrix{default=Default, symmetric=Sym}.
+new(Default,Boolean) when is_boolean(Boolean) -> 
+  #sparse_matrix{default=Default, symmetric=Boolean}.
 
-from_triplet(Triplets) -> from_triplet(Triplets, 0).
+
+from_triplet(Triplets) ->
+	from_list([ {{R,C},V} || {R,C,V} <- Triplets]).	
+
+%% @spec from_list(List,[{symmetric,false},{dims,{R,C}},{strict,false},
+%%                        {default,0}]) -> #sparse_matrix{}
+from_triplet(Triplets,Options) when is_list(Options)-> 
+	from_list([ {{R,C},V} || {R,C,V} <- Triplets ], Options);
+
+%% @deprecated
 from_triplet(Triplets, Default) ->
   from_list([ {{R,C},V} || {R,C,V} <- Triplets ], Default).
-from_triplet(Triplets, Default, Symmetry) ->
-	from_list([ {{R,C},V} || {R,C,V} <- Triplets ], Default,Symmetry).	
 
-from_list(List) -> from_list(List, 0).
+
+from_list(List) -> from_list(List, [{default,0},{symmetric,false}]).
+
+%% @spec from_list(List,[{symmetric,false},{dims,{R,C}},{strict,false},
+%%                        {default,0}]) -> #sparse_matrix{}
+from_list(List,Options) when is_list(Options)->
+	
+	DimsTest = proplists:get_value(dims, Options),
+	Dims =
+	case DimsTest of
+		{R,C} when is_integer(R) and is_integer(C) -> DimsTest;
+		_ ->
+			MaxR = max_rows(List, hd(List)),
+			MaxC = max_cols(List, hd(List)),
+			{MaxR, MaxC}
+	end,
+	
+	Default = proplists:get_value(default, Options,0),
+	
+	SymmetricTest = proplists:get_value(symmetric, Options,false),
+	Symmetric = if is_boolean(SymmetricTest) -> SymmetricTest; 
+							true-> false end,
+	Strict =false,
+	MatrixT = #sparse_matrix{ dims=Dims, default=Default, strict=Strict,
+														values=List, symmetric=Symmetric},							 	
+	Matrix = enforce_upper(MatrixT),
+  Matrix;
+
+%% @deprecated
 from_list(List, Default) ->
   MaxR = max_rows(List, hd(List)),
   MaxC = max_cols(List, hd(List)),
-  from_list(List, Default, false, {MaxR,MaxC}).
+  from_list(List, Default, {MaxR,MaxC}).
 
-from_list(List, Default,Symmetry) ->
-  MaxR = max_rows(List, hd(List)),
-  MaxC = max_cols(List, hd(List)),
-  from_list(List, Default, Symmetry, {MaxR,MaxC}).
-
-
-from_list(List, Default, Symmetry, {MaxR,MaxC})
-	when is_integer(MaxR), is_integer(MaxC) ->	
-  #sparse_matrix{dims={MaxR,MaxC}, default=Default, values=List, 
-								 symmetric=Symmetry}.
+%% @deprecated
+from_list(List, Default, {MaxR,MaxC}) when is_integer(MaxR), is_integer(MaxC) ->
+  #sparse_matrix{dims={MaxR,MaxC}, default=Default, values=List}.
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ACCESSORS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Get the get of a cell in the matrix
-get({R,C}, Matrix) when is_record(Matrix,sparse_matrix) ->
-  El = [Value || {{R1,C1},Value} <- Matrix#sparse_matrix.values, R1==R, C1==C],
-	
-	%look for symetric cell
-	El2 =
-    if 
-	  (El == []) andalso Matrix#sparse_matrix.symmetric ->
-		  [Value2 || {{R2,C2},Value2} <- Matrix#sparse_matrix.values, R2==C, C2==R];		
-		true -> El
-    end,
+get({R,C}, #sparse_matrix{symmetric=false}=Matrix) ->
+	El = get_value({R,C}, Matrix#sparse_matrix.values),
+  get(El, Matrix#sparse_matrix.default);
 
-  get(El2, Matrix#sparse_matrix.default);
+get({R,C}, #sparse_matrix{symmetric=true}=Matrix) ->
+	El = get_value({R,C}, Matrix#sparse_matrix.values,upper),
+	get(El, Matrix#sparse_matrix.default);
 
 get([], Default) -> Default;
 get([H|_], _) -> H.
@@ -57,20 +81,22 @@ get([H|_], _) -> H.
 %% Put a value into the matrix using triplet form. This replaces any existing
 %% value
 %% TODO Add a dimensional check
-put({R,C, V}, Matrix) when is_record(Matrix,sparse_matrix) ->
-  Values = lists:keystore({R,C}, 1, Matrix#sparse_matrix.values, {{R,C},V}),
-  Matrix#sparse_matrix{values=Values}.
+put({R,C,V}, #sparse_matrix{symmetric=false}=Matrix) ->  
+	Values = put_value({R,C,V}, Matrix#sparse_matrix.values),
+  Matrix#sparse_matrix{values=Values};
 
-delete({R,C}, Matrix) when is_record(Matrix,sparse_matrix) ->
-  
-  Values = lists:keydelete({R,C}, 1, Matrix#sparse_matrix.values),
-	Values2 =
-		if Matrix#sparse_matrix.symmetric ->			
-		  lists:keydelete({C,R}, 1, Values);
-		true ->
-			Values
-		end,
-	Matrix#sparse_matrix{values=Values2}.
+put({R,C,V}, #sparse_matrix{symmetric=true}=Matrix) ->
+	Values = put_value({R,C,V}, Matrix#sparse_matrix.values,upper),
+	Matrix#sparse_matrix{values=Values}.
+
+
+delete({R,C}, #sparse_matrix{symmetric=false}=Matrix) ->
+  Values = delete_value({R,C},Matrix#sparse_matrix.values),
+	Matrix#sparse_matrix{values=Values};
+
+delete({R,C}, #sparse_matrix{symmetric=true}=Matrix) ->
+	Values = delete_value({R,C}, Matrix#sparse_matrix.values,upper),	
+	Matrix#sparse_matrix{values=Values}.
  
 coordinates(Matrix) when is_record(Matrix, sparse_matrix) ->
   [ {R,C} || {{R,C}, _} <- Matrix#sparse_matrix.values ].
@@ -80,6 +106,14 @@ dimensions(Matrix) when is_record(Matrix, sparse_matrix) ->
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% OPERATIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+put_value({R,C,V},Values) ->
+  lists:keystore({R,C}, 1, Values, {{R,C},V}).
+
+put_value({R,C,V},Values,upper) ->
+	if R > C -> put_value({C,R,V},Values);
+	true -> put_value({R,C,V},Values)
+	end.
+
 add(A,B) when is_record(A,sparse_matrix), is_record(B,sparse_matrix) ->
   ASet = ordsets:from_list(coordinates(A)),
   BSet = ordsets:from_list(coordinates(B)),
@@ -98,6 +132,33 @@ mult(A, B) when is_record(A,sparse_matrix), is_record(B,sparse_matrix) ->
   not_implemented.
   
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PRIVATE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+delete_value({R,C},Values) ->
+  lists:keydelete({R,C}, 1, Values).
+delete_value({R,C},Values,upper) ->
+	if R > C -> delete_value({C,R},Values);
+	true -> delete_value({R,C},Values)
+	end.
+
+get_value({R,C},Values) ->
+   [Value || {{R1,C1},Value} <- Values, R1==R, C1==C].
+get_value({R,C},Values,upper) ->	
+	if R > C -> get_value({C,R},Values); 
+	true -> get_value({R,C},Values)
+	end.
+
+%enforce upper triangular form for symmetric matrices
+enforce_upper(#sparse_matrix{symmetric=false}=Matrix) ->
+ Matrix;
+enforce_upper(#sparse_matrix{symmetric=true}=Matrix) ->
+  Values = Matrix#sparse_matrix.values,	
+	NewValues =
+		lists:map(fun({{R,C},V}) ->
+					   if R > C -> {{C,R},V};
+						 true -> {{R,C},V}
+						 end								 
+						end,Values),
+	Matrix#sparse_matrix{values=NewValues}.
+
 %% Get the number of rows in the matrix
 max_rows(List, {{R0,_},_}) when is_integer(R0) ->
   hd(lists:reverse(lists:sort([R || {{R,_},_} <- List])));
--- test/sparse_matrix_tests.erl
+++ test/sparse_matrix_tests.erl
@@ -28,7 +28,7 @@ from_triplet_default_test_() ->
 from_triplet_symmetric_test_() ->
   Raw = [ {new_york,montreal,8}, {new_york,boston,4}, {new_york,buffalo,9},
     {buffalo,toronto,3}, {boston,montreal,7} ],
-	Mat = sparse_matrix:from_triplet(Raw, -1, true),
+	Mat = sparse_matrix:from_triplet(Raw, [{default,-1},{symmetric,true}]),
 	?_assertMatch({3,4}, sparse_matrix:dimensions(Mat)),
 	?_assertMatch(4, sparse_matrix:get({boston,new_york}, Mat)),
 	?_assertMatch(8, sparse_matrix:get({montreal,new_york},Mat)),
@@ -37,7 +37,7 @@ from_triplet_symmetric_test_() ->
 from_triplet_symmetric_false_test_() ->
   Raw = [ {new_york,montreal,8}, {new_york,boston,4}, {new_york,buffalo,9},
     {buffalo,toronto,3}, {boston,montreal,7} ],
-	Mat = sparse_matrix:from_triplet(Raw, -1, false),
+	Mat = sparse_matrix:from_triplet(Raw, [{default,-1},{symmetric,false}]),
   ?_assertMatch({3,4}, sparse_matrix:dimensions(Mat)),
   ?_assertMatch(4, sparse_matrix:get({new_york,boston}, Mat)),
 	?_assertMatch(-1, sparse_matrix:dimensions(Mat)),
@@ -84,11 +84,11 @@ delete_test_() ->
 
 delete_symmetry_test_() ->
   Raw = [ {1,1, 5}, {1,2, 3}, {2,1, 4}, {2,6, 3} ],
-  Mat = sparse_matrix:from_triplet(Raw,0,true),
+  Mat = sparse_matrix:from_triplet(Raw,[{default,0},{symmetric,true}]),
   Mat1 = sparse_matrix:put({2,5, 5}, Mat),
 	Mat2 = sparse_matrix:put({5,2, 5}, Mat1),
-  ?_assertMatch(5, sparse_matrix:get({2,5}, Mat1)),
-	?_assertMatch(5, sparse_matrix:get({5,2}, Mat1)),
+  ?_assertMatch(5, sparse_matrix:get({2,5}, Mat2)),
+	?_assertMatch(5, sparse_matrix:get({5,2}, Mat2)),
   Mat3 = sparse_matrix:delete({2,5}, Mat2),
 	?_assertMatch(0, sparse_matrix:get({2,5}, Mat3)),
 	?_assertMatch(0, sparse_matrix:get({5,2}, Mat3)).
